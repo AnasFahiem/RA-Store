73576b331939b226eddd6cfafc69df4d7bad5bbd
aaa <aaa>
2026-01-24 14:10:10 +0200

    Set default language to Arabic

--- i18n.ts
+++ i18n.ts
@@ -6,7 +6,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
     // Ensure that a valid locale is used
     if (!locale || !['en', 'ar'].includes(locale)) {
-        locale = 'en';
+        locale = 'ar';
     }
 
     return {
